7ad3cd5f3f2e5ffb156cd80fbc1c87561504bb12
Kevin Chambers <kc@kevinchambers.com>
2018-10-10 12:17:45 -0700

    Add env file for cdot version

---
+++ src/environments/environment.cdot.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  BASE_RIDEPILOT_URL: 'http://cdot-demo.ridepilot.io/api/v2/',
+  BASE_RIDEPILOT_AVL_URL: 'http://cdot-demo.ridepilot.io/ridepilot_cad_avl/api/v1/',
+  ACTION_CABLE_HOST: "ws://cdot-demo.ridepilot.io/cable",
+  GOOGLE_MAPS_KEY: "AIzaSyCjwh8RQbvJfP_4q198H-dVBJhR4OFi_zs"
+};
